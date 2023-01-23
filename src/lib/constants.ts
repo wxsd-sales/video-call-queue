4cef9a3d6968fe68e975415d3ed6f1b05d4542b3
Arash Koushkebaghi <arash.koush@gmail.com>
2023-01-23 15:39:01 -0800

    feat: deploy to prod

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,3 +4,7 @@ export const WEBEX_SDK_CONFIG = {
   logger: { level: 'silent' },
   meetings: { reconnection: { enabled: true } }
 };
+
+
+export const VALID_ACCESS_TOKEN =
+  /^([a-zA-Z0-9]{64})_(.*)_([0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12})$/;
