8aba8f89b62e05ccd9ec338078dae129297fe892
Arash Koushkebaghi <arash.koush@gmail.com>
2023-12-05 20:29:09 -0800

    feat: fixed image size

--- src/components/Card/Card.svelte
+++ src/components/Card/Card.svelte
@@ -40,15 +40,15 @@
 </script>
 
 <div id={index} class="card is-translucent-black p-3">
-  <div class="card-image">
-    <figure class="image is-5by3">
-      <img src={img} alt="support-figure" />
+  <div class="card-image container" style="width: 18rem">
+    <figure class="image is-3by2">
+      <img src={img} alt="support-figure" class="is-fullwidth" />
     </figure>
   </div>
   <div class="card-content">
     <div class="content">
       <button
-        class="button is-size-4 is-primary is-centered  {buttonIsLoading && 'is-loading'}"
+        class="button is-size-5 is-primary is-centered is-fullwidth {buttonIsLoading && 'is-loading'}"
         disabled={!isDevice}
         on:click={submitRequest}
         >{title}
