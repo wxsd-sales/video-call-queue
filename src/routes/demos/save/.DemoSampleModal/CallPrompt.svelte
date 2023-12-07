623425cbd33a612c0d05fe1db0bf6cb1e9734d69
Arash Koushkebaghi <arash.koush@gmail.com>
2023-12-07 15:09:46 -0800

    fix: center card

--- src/routes/demos/save/.DemoSampleModal/CallPrompt.svelte
+++ src/routes/demos/save/.DemoSampleModal/CallPrompt.svelte
@@ -3,19 +3,20 @@
   export let sipImage: string;
 </script>
 
-<div class="card is-translucent-black p-3">
-  <div class="container card-image">
-    <figure class="image  is-3by2">
-      <img src={sipImage} alt="support-figure" />
-    </figure>
-  </div>
-  <div class="card-content">
-    <div class="content">
-      <button class="button fixed is-size-6 is-primary is-centered is-fullwidth" disabled>{title} </button>
+<div>
+  <div class="card is-translucent-black p-3">
+    <div class="container card-image">
+      <figure class="image  is-3by2">
+        <img src={sipImage} alt="support-figure" />
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="content">
+        <button class="button fixed is-size-6 is-primary is-centered is-fullwidth" disabled>{title} </button>
+      </div>
     </div>
   </div>
 </div>
-å
 
 <style>
   .fixed {
