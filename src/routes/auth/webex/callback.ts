a91ff9e1aee1e51070e3f13b788ec7c7663d8106
Arash Koushkebaghi <arash.koush@gmail.com>
2024-01-17 13:46:59 -0800

    WIP

--- src/routes/auth/webex/callback.ts
+++ src/routes/auth/webex/callback.ts
@@ -1,11 +1,7 @@
 import type { RequestEvent } from '@sveltejs/kit';
 import type { JSONObject } from '@sveltejs/kit/types/private';
-import type { TokenResponse } from '$lib/types';
-import { urlEncodedRequest } from '$lib/shared/urlencoded-request';
-import { webexHttpPeopleResource } from '$lib/webex/http-wrapper';
 import { Session, User } from '../../../database/entities';
 import { prerendering } from '$app/env';
-import humps from 'humps';
 import env from '$lib/environment';
 import * as cookie from 'cookie';
 import { getUserUUID } from '$lib/webex/common';
@@ -33,57 +29,50 @@ export const GET = async (requestEvent: RequestEvent) => {
   });
 
   const db = requestEvent.locals.db;
-  const params = humps.decamelizeKeys({
-    grantType,
-    clientId: env.WEBEX_AUTHORIZATION_CODE_CLIENT_ID,
-    clientSecret: env.WEBEX_AUTHORIZATION_CODE_CLIENT_SECRET,
-    code,
-    redirectUri
+  const body = `grant_type=${grantType}&client_id=${env.WEBEX_AUTHORIZATION_CODE_CLIENT_ID}&client_secret=${env.WEBEX_AUTHORIZATION_CODE_CLIENT_SECRET}&redirect_uri=${redirectUri}&code=${code}`;
+
+  try{ 
+
+  const token = await (await fetch(`${env.WEBEX_API_URL}/access_token` , {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/x-www-form-urlencoded',
+      }),
+      body
+    })).json();
+
+  addExpiresAts(token, Date.parse(new Date().toUTCString()));
+  const me = await (await fetch(`${env.WEBEX_API_URL}/people/me`, {
+    headers: new Headers({
+      'Authorization': `Bearer ${token.access_token}`,
+      'Content-Type': 'application/json'
+    })
+  })).json();
+
+  const user = await db?.findOne(User, { uuid: getUserUUID(me.id) })
+  const session = new Session({
+    user: user ?? new User(getUserUUID(me.id), me.emails[0]),
+    ipAddress: prerendering ? 'unknown' : requestEvent.clientAddress,
+    userAgent: requestEvent.request.headers.get('User-Agent') ?? undefined,
+    lastActivityAt: Date.now(),
+    payload: { webex: { ...me, orgId: me.orgId } }
   });
-  const tokenResponse = urlEncodedRequest(env.WEBEX_API_URL)
-    .post('access_token', params as Record<string, string>)
-    .then((r) =>
-      r
-        .json()
-        .then((s: JSONObject) => humps.camelizeKeys(s) as JSONObject)
-        .then((s: JSONObject) => addExpiresAts(s, Date.parse(r.headers.get('date') ?? new Date().toUTCString())))
-    ) as Promise<TokenResponse>;
+  const sessionCookie = cookie.serialize('sessionId', session.uuid, {
+    path: '/',
+    maxAge: 60 * 60 * 24 * 7,
+    sameSite: 'lax'
+  });
 
-  return await tokenResponse
-    .then((r: TokenResponse) =>
-      webexHttpPeopleResource(r.accessToken)
-        .getMyOwnDetails()
-        .then((s: Response) => s.json() as Promise<PersonResponse>)
-        .then((s: PersonResponse) =>
-          db != null
-            ? db.findOne(User, { uuid: getUserUUID(s.id) }).then(async (t) => {
-                const session = new Session({
-                  user: t ?? new User(getUserUUID(s.id), s.emails[0]),
-                  ipAddress: prerendering ? 'unknown' : requestEvent.clientAddress,
-                  userAgent: requestEvent.request.headers.get('User-Agent') ?? undefined,
-                  lastActivityAt: Date.now(),
-                  payload: { webex: { ...r, orgId: s.orgId } }
-                });
-                const sessionCookie = cookie.serialize('sessionId', session.uuid, {
-                  path: '/',
-                  maxAge: 60 * 60 * 24 * 7,
-                  sameSite: 'lax'
-                });
-                await db.persistAndFlush(session);
+  await db?.persistAndFlush(session);
 
-                return {
-                  status: 302,
-                  headers: {
-                    'Location': `${env.PUBLIC_TUNNEL ? env.PUBLIC_TUNNEL : ''}/demos`,
-                    'Set-Cookie': sessionCookie
-                  }
-                };
-              })
-            : { status: 403 }
-        )
-    )
-    .catch((e) => {
-      console.log(e);
-      return { status: 403 };
-    });
+  return {
+    status: 302,
+    headers: {
+      'Location': `${env.PUBLIC_TUNNEL ? env.PUBLIC_TUNNEL : ''}/demos`,
+      'Set-Cookie': sessionCookie
+    }
+  }} catch(e) {
+    return { status: 403 };
+  };
 };
+
